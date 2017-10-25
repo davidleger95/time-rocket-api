fed60a054de93a323c862d2baadc9a3181300ff0
Leger, David <DLeger@cvent.com>
2017-10-25 10:50:49 -0300

    stuff

--- schema/TimesheetType.js
+++ schema/TimesheetType.js
@@ -9,7 +9,6 @@ const UserType = new GraphQLObjectType({
   description: 'Generic User Model',
   fields: () => ({
     _id: { type: GraphQLString },
-    id: { type: GraphQLString },
     imageUrl: { type: GraphQLString },
     firstName: { type: GraphQLString },
     lastName: { type: GraphQLString },
@@ -18,7 +17,6 @@ const UserType = new GraphQLObjectType({
     status: { type: GraphQLString },
     startDate: { type: GraphQLString },
     userRole: { type: GraphQLString }
-    //timesheets: { type: TimesheetType } // arrayOf
   })
 });
 
